42106fb6a2c0237852d241e57e3b0fbd293aae84
Bogdan <bogdan.rubanik28@gmail.com>
2025-11-19 23:14:47 +0700

    [feature]new errors

--- cmd/user-service/internal/errors/errors.go
+++ cmd/user-service/internal/errors/errors.go
@@ -3,9 +3,16 @@ package errors
 import "errors"
 
 var (
-	ErrEmailAlreadyExists    = errors.New("email уже занят")
-	ErrUsernameAlreadyExists = errors.New("username уже занят")
-	ErrInvalidCredentials    = errors.New("неверные учетные данные")
-	ErrUserNotFound          = errors.New("пользователь не найден")
-	ErrInvalidInput          = errors.New("неверные данные запроса")
+	ErrEmailAlreadyExists    = errors.New("email is already taken")
+	ErrUsernameAlreadyExists = errors.New("username is already taken")
+	ErrInvalidCredentials    = errors.New("incorrect credentials")
+	ErrUserNotFound          = errors.New("user not found")
+	ErrInvalidInput          = errors.New("invalid request data")
+
+	ErrUsernameRequired   = errors.New("username is required")
+	ErrEmailRequired      = errors.New("email is required")
+	ErrPasswordRequired   = errors.New("password is required")
+	ErrPasswordTooShort   = errors.New("password must be at least 6 characters")
+	ErrInvalidEmailFormat = errors.New("invalid email format")
+	ErrPhoneTooShort      = errors.New("phone number is too short")
 )
--- cmd/user-service/internal/service/validation/validation.go
+++ cmd/user-service/internal/service/validation/validation.go
@@ -2,9 +2,9 @@ package validation
 
 import (
 	"context"
-	"errors"
 	"strings"
 	"user-service/internal/dto/request"
+	"user-service/internal/errors"
 	"user-service/internal/repository"
 )
 
@@ -19,27 +19,26 @@ func NewUserValidation(countryRepo *repository.UserRepository) *UserValidation {
 }
 
 func (v *UserValidation) ValidateCreateRequest(ctx context.Context, req *request.Create) error {
-	// Базовые проверки
 	if strings.TrimSpace(req.Username) == "" {
-		return errors.New("username is required")
+		return errors.ErrUsernameRequired
 	}
 	if strings.TrimSpace(req.Email) == "" {
-		return errors.New("email is required")
+		return errors.ErrEmailRequired
 	}
 	if strings.TrimSpace(req.Password) == "" {
-		return errors.New("password is required")
+		return errors.ErrPasswordRequired
 	}
 
 	if len(req.Password) < 6 {
-		return errors.New("password must be at least 6 characters")
+		return errors.ErrPasswordTooShort
 	}
 
 	if !strings.Contains(req.Email, "@") || !strings.Contains(req.Email, ".") {
-		return errors.New("invalid email format")
+		return errors.ErrInvalidEmailFormat
 	}
 
 	if req.Phone != "" && len(req.Phone) < 5 {
-		return errors.New("phone number is too short")
+		return errors.ErrPhoneTooShort
 	}
 
 	return nil
